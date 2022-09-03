dd51b5c542c6d5f26137f5f0b2e9a624e1dbb6fb
Jõnathas Gouveia <jonathas_gouv@hotmail.com>
2022-09-03 16:56:36 -0300

    change weekly topic to 5pm

--- src/schedule.ts
+++ src/schedule.ts
@@ -3,4 +3,4 @@ import reminder from '@crons/reminder'
 import members from '@crons/members'
 
 cron.schedule('* * * * *', reminder.sendResponses, { scheduled: true, timezone: 'America/Sao_Paulo' })
-cron.schedule('0 16 * * SAT', () => members.mostPosts(8), { scheduled: true, timezone: 'America/Sao_Paulo' })
+cron.schedule('0 17 * * SAT', () => members.mostPosts(8), { scheduled: true, timezone: 'America/Sao_Paulo' })

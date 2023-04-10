effd9a3a62c2b51555c7b01a441772354a78b756
Jõnathas Gouveia <jonathas_gouv@hotmail.com>
2023-04-10 16:45:56 -0300

    creates dockerfile and changes default port to 8080

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from './app'
 
-app.listen(process.env.PORT || 3333, () => {
-	console.log(`server is running in port ${process.env.PORT || 3333}`)
+app.listen(process.env.PORT || 8080, () => {
+	console.log(`server is running in port ${process.env.PORT || 8080}`)
 })
